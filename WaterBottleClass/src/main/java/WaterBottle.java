4c098e9628e6c70da641f776da99d2bcfdccc605
Becky Entwistle <96063238+bjentwistle@users.noreply.github.com>
2023-06-19 23:53:34 +0100

    Update WaterBottle.java

--- WaterBottleClass/src/main/java/WaterBottle.java
+++ WaterBottleClass/src/main/java/WaterBottle.java
@@ -1,23 +1,24 @@
 public class WaterBottle {
     private int volume = 100;
+// initialise volume to 100
 
     public WaterBottle () {
         this.volume = volume;
-    }
+    }// add volume attribute to WaterBottle
 
     public int getVolume() {
         return this.volume;
-    }
+    }//create getter method get volume
 
     public void drink() {
         this.volume -= 10;
-    }
+    }//create drink method to reduce volume
 
     public void empty() {
         this.volume = 0;
-    }
+    }// create emoty method
 
     public void fill() {
         this.volume = 100;
-    }
+    }// create fill method. 
 }
